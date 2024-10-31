eb8ac7c18d2d02d9a42d556a1c4b4b76abb9872e
SpPag <spiridonpagiatis@gmail.com>
2024-10-31 12:43:45 +0200

    Update Student_class.py

--- Student_class.py
+++ Student_class.py
@@ -19,7 +19,7 @@ So with that we have created a new class, that we have named "Student". The "__i
 function. It's needed to create the new data type. The "self" part wasn't explained by Tutorial guy but I guess it's
 mandatory. The rest of the elements inside the parentheses are basically prompts for the information the function needs.
 Below that, with all the "self.name" etc stuff we are assigning the values that the prompts are given by the coder to
-the function so that it can run. Following "Tutorial guy"'s example, I will import this into another file to run it
+the function so that it can run. Following Tutorial guy's example, I will import this into another file to run it
 there. It is good practice I think to keep the "import" command in mind and also this time we'll see how to grab a
 piece of information from a specific file. 
 '''
