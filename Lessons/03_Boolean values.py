11d1c594a40ae3fc445ecbfa9c2c9441d6701506
SpPag <spiridonpagiatis@gmail.com>
2024-10-31 12:19:57 +0200

    Update 03_Boolean values.py

--- Lessons/03_Boolean values.py
+++ Lessons/03_Boolean values.py
@@ -1,8 +1,8 @@
 is_male = False
 if is_male == True:
-    print("Penis")
+    print("Dude")
 else:
-    print("Vagina")
+    print("Dudess")
 
 ''' In cases like the above you don't need the " == True". You can just code "if is_male:" and it treats it as if you're
 testing whether it's true.
